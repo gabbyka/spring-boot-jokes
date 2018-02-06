a67974042f47cf3f8f3a5875c278a545f11bbe76
gn <gn@star-trac.de>
2018-02-06 16:48:56 +0100

    - step 2 ! buggyy!!!

---
+++ src/main/java/guru/springframework/spring5jokes/services/ChuckService.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring5jokes.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChuckService {
+    
+    //@Autowired
+    //private ChuckNorrisQuotes chuckNorrisQuotes;
+}
