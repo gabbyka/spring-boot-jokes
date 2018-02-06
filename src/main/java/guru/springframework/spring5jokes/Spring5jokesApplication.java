21ed13b8a4a1d33dcd0899353a835441c824eac7
gn <gn@star-trac.de>
2018-02-06 15:45:18 +0100

    - initial commit

---
+++ src/main/java/guru/springframework/spring5jokes/Spring5jokesApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring5jokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5jokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5jokesApplication.class, args);
+	}
+}
